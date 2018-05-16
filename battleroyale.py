341a03d95008a3d5273d625802b444f3eef14c29
Luke Josten <lukejosten@gmail.com>
2018-05-16 16:51:47 -0400

    Added testing output, changed the test cases, and made it work with multiple test cases

--- battleroyale.py
+++ battleroyale.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python
 import sys
 
-
-numEnemies = int(sys.stdin.readline())
-lines = sys.stdin.read().splitlines()
-enemyList = []
-for line in lines:
-    line = line.split()
-    enemyList.append(map(int,line))
-
-minAmmo = 0
-ammo = enemyList[0][0]
-
 def findMinAmmo():
     global ammo, ammoList, minAmmo
     ammoList =[ammo]
@@ -28,10 +17,28 @@ def findMinAmmo():
             ammo = ammoList.pop(0) + (enemy[0] - ammoList.pop(len(ammoList) - 1))
             findMinAmmo()
 
+numTests = int(sys.stdin.readline())
+lines = sys.stdin.readlines()
+testIndex = 0
 
-findMinAmmo()
+for i in range(numTests):
+    totalEnemies = int(lines[testIndex])
 
-print(minAmmo)
+    enemyList = []
+    for j in range(1, totalEnemies + 1):
+        enemy = lines[testIndex + j].split(" ")
+        enemyList.append([int(enemy[0]), int(enemy[1])])
+
+    minAmmo = 0
+    ammo = enemyList[0][0]
+    ammoCopy = ammo
+
+    findMinAmmo()
+    if minAmmo == 0:
+        print(ammoCopy)
+    else:
+        print(minAmmo)
+    testIndex += totalEnemies + 1
 
 
 
--- generate_tests.py
+++ generate_tests.py
@@ -2,12 +2,15 @@ import random
 
 def main():
     f = open("testing_input.txt", "w+")
-    n = 100
-    f.write(str(n) + "\n")
-    for i in range(n):
-        health = random.randrange(1, 200, 1)
-        ammo = random.randrange(0, 200, 1)
-        f.write(str(health) + " " + str(ammo) + "\n")
+    t = 19
+    f.write(str(t) + "\n")
+    for i in range(t):
+        n = random.randrange(1, 100, 1)
+        f.write(str(n) + "\n")
+        for i in range(n):
+            health = random.randrange(1, 200, 1)
+            ammo = random.randrange(0, 200, 1)
+            f.write(str(health) + " " + str(ammo) + "\n")
     f.close()
 
 main()
